eb4d483dae1b5c590327d33340952acef17a5f17
Tajul <subqitajul7@gmail.com>
2024-06-03 00:36:45 +0700

    fix: private route

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,10 +1,20 @@
 "use client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import React from "react"
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
 
 const queryClient = new QueryClient()
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (!token) {
+      router.push("/login")
+    }
+  }, [router])
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
 
