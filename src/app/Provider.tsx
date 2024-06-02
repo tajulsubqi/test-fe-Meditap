ce25011e32ec0feb0dcbac457fdd60766a6dfe04
Tajul <subqitajul7@gmail.com>
2024-06-03 00:40:21 +0700

    fix error

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,20 +1,10 @@
 "use client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { useRouter } from "next/router"
-import React, { useEffect } from "react"
+import React from "react"
 
 const queryClient = new QueryClient()
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter()
-
-  useEffect(() => {
-    const token = localStorage.getItem("token")
-    if (!token) {
-      router.push("/login")
-    }
-  }, [router])
-
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
 
